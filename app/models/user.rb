72b51b4c877166ccd81387354e4bb60d6b7c236a
Shaibu Emmanuel <emmagd4@gmail.com>
2017-07-30 18:22:46 +0100

    add authentication to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
 
   before_save { self.email = email.downcase }
 
+  has_secure_password
+
   validates :username, presence: true,
             uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 25 }
