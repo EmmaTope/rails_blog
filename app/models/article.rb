c675ac8b65dbd59babfe6aed9e3541fb3321aa25
Shaibu Emmanuel <emmagd4@gmail.com>
2017-07-30 04:03:02 +0100

    add association between user and artlce

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ActiveRecord::Base
+  belongs_to :user
+
   validates :title, length: { in: 3..50 }
   validates :description, presence: true, length: { in: 10..500 }
-  
+  validates :user_id, presence: true
+
 end
